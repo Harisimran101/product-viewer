8296189c957cebbe5c6ec4110dbfcf3368bb54b0
Harisimran101 <harisimran1012@gmail.com>
2022-03-15 13:02:09 +0500

    update

---
+++ iphone-data.js
@@ -0,0 +1,18 @@
+let data = {
+     autorotate: false,
+     rotatespeed: 4.0,
+     stop: false,
+     animate: false,
+     duration: 2, // time in seconds
+     graphics: 'high',
+}
+
+let modeldata = {
+     scale: 1, // default is 1
+     product_link: "",
+     Animation: true,
+     duration: 5, // seconds
+     delay: 2, // seconds
+     enableShadow: true,
+}
+
